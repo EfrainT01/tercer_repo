f2d0343ab1894d5177f79029a6be412291ee53b4
Jhontan Tarazona <efrain.tarazona-vargas@inetum.com>
2026-09-11 18:49:08 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Un paquete pip simple de saludo",
+    author="Efrain Tarazona",
+    author_email="pruebasd566@gmail.com ",
+    url="https://github.com/EfrainT01/tercer_repo",
+)
